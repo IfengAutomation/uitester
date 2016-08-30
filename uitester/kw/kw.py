2484cb1f4163dc0357e28aa36aa581bb22a64cff
zhaoye <zy81218@gmail.com>
2016-08-30 19:53:41 +0800

    refector kw

--- uitester/kw/kw.py
+++ uitester/kw/kw.py
@@ -8,7 +8,6 @@ from types import FunctionType
 
 script_dir = dirname(abspath(__file__))
 libs_dir = join(join(join(script_dir, pardir), pardir), 'libs')
-print('Add path {}'.format(libs_dir))
 sys.path.append(libs_dir)
 
 
@@ -17,114 +16,67 @@ logger = logging.getLogger()
 logger.setLevel(logging.WARNING)
 
 
-AS = 'as'
-QUOTE = '"'
-SPACE = ' '
-COMMENT = '#'
-DOT = '.'
+class KWCore:
+    AS = 'as'
+    QUOTE = '"'
+    SPACE = ' '
+    COMMENT = '#'
+    DOT = '.'
 
+    def __init__(self):
+        self.user_var = {}
+        self.default_func = {'import': self.kw_import, 'check': self.kw_check, 'print': self.kw_print}
+        self.user_func = {**self.default_func}
+        self.parsed_line = []
+        self.line_count = 0
 
-def get_value(word):
-    if word in user_var:
-        return user_var[word]
-
-    dot_count = word.count(DOT)
-    if dot_count > 1:
-        raise ValueError('Unsupported usage {}'.format(word))
-    elif dot_count == 1:
-        var_name, attr_name = word.split(DOT)
-        if var_name in user_var:
-            var = user_var[var_name]
-            if hasattr(var, attr_name):
-                return getattr(var, attr_name)
-            else:
-                raise ValueError('{} not has attr {}'.format(var_name, attr_name))
-        else:
-            raise ValueError('Var {} not found'.format(var_name))
-    else:
-        return word
-
-
-def kw_import(*args, **kwargs):
-    logger.debug('Import kw lib {}'.format(args))
-    for lib_name in args:
-        module = __import__(lib_name)
-        for attr_name in dir(module):
-            if attr_name.startswith('__'):
-                continue
-            attr = getattr(module, attr_name)
-            if type(attr) is FunctionType:
-                user_func[attr_name] = attr
-
-
-def kw_check(*args, **kwargs):
-    logger.debug('Check {}'.format(args))
-    if len(args) < 2:
-        raise ValueError('Check need 2 arguments. but {} given'.format(len(args)))
-
-    assert get_value(args[0]) == get_value(args[1])
-
-
-def kw_print(*args, **kwargs):
-    output = []
-    for arg in args:
-        if arg in user_func:
-            output.append('<KW:{}>'.format(arg))
-        elif arg in user_var:
-            output.append('<Var:{}={}>'.format(arg, user_var[arg]))
-        elif arg.count(DOT) == 1:
-            var_name, attr = arg.split(DOT)
-            if var_name in user_var:
-                var = user_var[var_name]
-                if hasattr(var, attr):
-                    attr_value = getattr(var, attr)
-                    output.append(attr_value)
-                else:
-                    output.append('<Var:{} not found attr {}>'.format(var, attr))
-            else:
-                output.append('<Var:{} not found>'.format(var))
-        else:
-            output.append(arg)
-    print(*output)
-
-
-user_func = {'import': kw_import, 'check': kw_check, 'print': kw_print}
-user_var = {}
-
-
-class KWLine:
-    """
-    Keywords line
-    """
-    def __init__(self, line_number=0, items=None, var=None, raw=None):
-        self.line_number = line_number
-        self.items = items
-        self.func = None
-        self.var = var
-        self.comment = False
-        self.raw = raw
-
-    def prepare(self):
-        # TODO check kw line
-        if self.items[0] not in user_func:
-            raise ValueError('keyword "{}" not found!'.format(self.items[0]))
-
-    def execute(self):
-        if self.comment:
+    def _execute_line(self, kw_line):
+        if kw_line.comment:
             return
 
-        logger.debug('exec items {}'.format(self.items))
-        if self.items[0] not in user_func:
-            raise ValueError('keyword "{}" not found!'.format(self.items[0]))
+        logger.debug('exec items {}'.format(kw_line.items))
 
-        res = user_func[self.items[0]](*self.items[1:])
-        if self.var:
-            user_var[self.var] = res
+        res = self.user_func[kw_line.items[0]](*kw_line.items[1:])
+        if kw_line.var:
+            self.user_var[kw_line.var] = res
 
-    @classmethod
-    def parse_line(cls, kw_line, line_number=0):
-        line = cls(raw=kw_line, line_number=line_number)
-        if kw_line.strip().startswith(COMMENT):
+    def parse_line(self, line):
+        # parse line to kw line
+        kw_line = self._parse_line(line)
+
+        func = kw_line.items[0]
+
+        if func == 'import':
+            # pre import lib
+            self.default_func['import'](*kw_line.items[1:])
+
+        if func not in self.user_func and func not in self.user_var:
+            raise ValueError('Define not found {}'.format(func))
+
+        if kw_line.var:
+            self.user_var[kw_line.var] = None
+
+        # set line number
+        self.line_count += 1
+        kw_line.line_number = self.line_count
+        # add kw line to cache
+        self.parsed_line.append(kw_line)
+
+    def execute(self):
+        # run all kw line in cache
+        for line in self.parsed_line:
+            self._execute_line(line)
+
+    def release(self):
+        # release all var and func
+        self.user_var.clear()
+        self.user_func.clear()
+        self.user_func = {**self.default_func}
+        self.parsed_line = []
+
+    def _parse_line(self, kw_line, line_number=0):
+        line = KWLine(raw=kw_line, line_number=line_number)
+        if kw_line.strip().startswith(self.COMMENT):
             line.comment = True
             return line
 
@@ -133,10 +85,10 @@ class KWLine:
         in_quotation = False
         var = None
         for char in kw_line:
-            if char == SPACE and not in_quotation and cache:
+            if char == self.SPACE and not in_quotation and cache:
                 kw_items.append(cache)
                 cache = None
-            elif char == QUOTE:
+            elif char == self.QUOTE:
                 in_quotation = not in_quotation
             else:
                 if not cache:
@@ -149,8 +101,8 @@ class KWLine:
         if in_quotation:
             raise ValueError('Missing quote. {}'.format(kw_line))
 
-        if AS in kw_items:
-            as_index = kw_items.index(AS)
+        if self.AS in kw_items:
+            as_index = kw_items.index(self.AS)
             if as_index < (len(kw_items) - 2):
                 raise ValueError('Keywords "as" should only set one variable')
             elif as_index == (len(kw_items) - 1):
@@ -163,18 +115,74 @@ class KWLine:
         line.var = var
         return line
 
+    def get_value(self, word):
+        if word in self.user_var:
+            return self.user_var[word]
 
-def start_test_cmd():
+        dot_count = word.count(self.DOT)
+        if dot_count > 1:
+            raise ValueError('Unsupported usage {}'.format(word))
+        elif dot_count == 1:
+            var_name, attr_name = word.split(self.DOT)
+            if var_name in self.user_var:
+                var = self.user_var[var_name]
+                if hasattr(var, attr_name):
+                    return getattr(var, attr_name)
+                else:
+                    raise ValueError('{} not has attr {}'.format(var_name, attr_name))
+            else:
+                raise ValueError('Var {} not found'.format(var_name))
+        else:
+            return word
 
-    while True:
-        cmd_line = input('>>')
-        if cmd_line == '':
-            continue
-        elif cmd_line == 'quit':
-            break
-        kw_line = KWLine.parse_line(cmd_line)
-        if not kw_line.comment:
-            kw_line.execute()
+    def kw_import(self, *args, **kwargs):
+        logger.debug('Import kw lib {}'.format(args))
+        for lib_name in args:
+            module = __import__(lib_name)
+            for attr_name in dir(module):
+                if attr_name.startswith('__'):
+                    continue
+                attr = getattr(module, attr_name)
+                if type(attr) is FunctionType:
+                    self.user_func[attr_name] = attr
 
-if __name__ == '__main__':
-    start_test_cmd()
+    def kw_check(self, *args, **kwargs):
+        logger.debug('Check {}'.format(args))
+        if len(args) < 2:
+            raise ValueError('Check need 2 arguments. but {} given'.format(len(args)))
+
+        assert self.get_value(args[0]) == self.get_value(args[1])
+
+    def kw_print(self, *args, **kwargs):
+        output = []
+        for arg in args:
+            if arg in self.user_func:
+                output.append('<KW:{}>'.format(arg))
+            elif arg in self.user_var:
+                output.append('<Var:{}={}>'.format(arg, self.user_var[arg]))
+            elif arg.count(self.DOT) == 1:
+                var_name, attr = arg.split(self.DOT)
+                if var_name in self.user_var:
+                    var = self.user_var[var_name]
+                    if hasattr(var, attr):
+                        attr_value = getattr(var, attr)
+                        output.append(attr_value)
+                    else:
+                        output.append('<Var:{} not found attr {}>'.format(var, attr))
+                else:
+                    output.append('<Var:{} not found>'.format(var))
+            else:
+                output.append(arg)
+        print(*output)
+
+
+class KWLine:
+    """
+    Keywords line
+    """
+    def __init__(self, line_number=0, items=None, var=None, raw=None):
+        self.line_number = line_number
+        self.items = items
+        self.var = var
+        self.comment = False
+        self.raw = raw
