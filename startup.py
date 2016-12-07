dc099557cac625dd85c2ff5ce61dd7682f4086f4
zhaoye <zy81218@gmail.com>
2016-12-07 13:30:51 +0800

    Remove: cmd mode

--- startup.py
+++ startup.py
@@ -1,6 +1,5 @@
 import sys
 import logging
-from uitester import cmd_client
 from uitester import ui_client
 
 logger = logging.getLogger('UiTester')
@@ -10,8 +9,7 @@ logger.setLevel(logging.DEBUG)
 def start():
     if len(sys.argv) > 1:
         if sys.argv[1] == 'cmd':
-            logger.debug('Start with cmd mode')
-            cmd_client.start()
+            print('Unsupported cmd')
             return
     logger.debug('Start with ui mode')
     ui_client.start()
